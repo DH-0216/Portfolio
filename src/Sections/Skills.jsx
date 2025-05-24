ccfd16a07a6b83a9943391f50408acc64143beb5
Dulaj Hashmika <dulajhashmika075@gmail.com>
2025-05-24 23:26:56 +0530

    update

--- src/Sections/Skills.jsx
+++ src/Sections/Skills.jsx
@@ -2,29 +2,43 @@ import React from "react";
 import TitleHeader from "../components/TitleHeader";
 import { bulb } from "../assets";
 import { skills } from "../utils";
+import { motion } from "motion/react";
+import { fadeInUp } from "../utils/motion";
 
 const Skills = () => {
   return (
-    <div className="mt-20 mx-10">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.3 }}
+      variants={fadeInUp()}
+      className="mt-20 mx-10"
+    >
       <TitleHeader
         title="Tools that drive my work"
         sub={
-          <span className="flex items-center gap-2 justify-center">
-            <img src={bulb} alt="bulb" className="size-5 inline-block" /> My Skills
+          <span className="flex items-center gap-3 justify-center">
+            <img src={bulb} alt="bulb" className="size-5 inline-block" /> My
+            Skills
           </span>
         }
       />
-      <div className="flex flex-row flex-wrap justify-center items-center my-10 gap-10 p-10">
+      <div className="flex flex-row flex-wrap justify-center items-center mt-16 gap-10 p-10 ">
         {skills.map((skill, idx) => (
-          <img
+          <motion.img
             key={skill.alt + idx}
             src={skill.src}
             alt={skill.alt}
             className={`size-18 ${skill.className || ""}`}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: false, amount: 0 }}
+            transition={{ duration: 0.6, ease: "easeOut", delay: idx * 0.1 }}
+            whileHover={{ scale: 1.1 }}
           />
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
